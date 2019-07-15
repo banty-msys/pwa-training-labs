0121b3d999e0c4c0569ad4507913511fad0d9fa0
Biswajeet <bmaharana@msystechnologies.com>
2019-07-15 15:29:09 +0530

    first commit

---
+++ cache-api-lab/app/sw.js
@@ -0,0 +1,51 @@
+const filesToCache = [
+    '/',
+    'images/still_life_medium.jpg'
+]
+
+const staticCacheName = 'pages-cache-v1'
+
+self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets')
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache)
+            })
+    )
+})
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url)
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache')
+                    return response
+                }
+                console.log('Network request for ', event.request.url)
+                return fetch(event.request)
+                // Add fetched files to the cache
+            })
+            .catch(error => {
+                // Respond with custom offline page
+            })
+    )
+})
+
+self.addEventListener('activate', event => {
+    console.log('Activating new service worker...')
+    const cacheWhitelist = [staticCacheName]
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName)
+                    }
+                })
+            )
+        })
+    )
+})
